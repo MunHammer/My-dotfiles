8e8e180e3e277606a7da5c72387ffc2727f8dade
MunHammer <munhammer@munhammer.com>
2026-08-05 21:42:41 +1200

    Added a basic nix configuration
    This was done using home-manager
    - Moved manually managed packages (~/.local/bin) to nix
    - Started moving dotfile management to nix

---
+++ home.nix
@@ -0,0 +1,51 @@
+{ pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = builtins.getEnv "USER";
+  home.homeDirectory = "/home/${builtins.getEnv "USER"}";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "26.05"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = with pkgs; [
+    nixd
+    nixfmt
+    shfmt
+    superhtml
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  xdg.configFile = {
+    "fastfetch/config.jsonc".source = ~/.dotfiles/fastfetch.jsonc;
+    "btop/btop.conf".source = ~/.dotfiles/btop.conf;
+    "starship.toml".source = ~/.dotfiles/starship.toml;
+    "zellij/config.kdl".source = ~/.dotfiles/zellij.kdl;
+  };
+  home.file = {
+    ".bashrc".source = ~/.dotfiles/bashrc;
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
